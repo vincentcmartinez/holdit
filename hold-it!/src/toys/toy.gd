e525279e5ec875e2d5cbad92b6a9836fb741a7c0
viamill <126583359+viamill@users.noreply.github.com>
2025-03-20 22:01:47 -0400

    basic button

--- hold-it!/src/toys/toy.gd
+++ hold-it!/src/toys/toy.gd
@@ -1,5 +1,18 @@
 extends RigidBody2D
 class_name Toy
 
+@onready var paused : bool = false
+
+func _ready():
+	add_to_group("Toy")
+
 func _physics_process(delta: float) -> void:
-	position.x += 1
+	if(!paused):
+		position.x += 1
+
+func pause():
+	paused = true
+	
+	###
+	### code to run minigame?
+	###
