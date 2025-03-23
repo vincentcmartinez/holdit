79adb362e7b06b3d6b714e6f14405cd601103ae6
Arti-Dev <boomcreeper296@gmail.com>
2025-03-23 14:32:03 -0400

    Conveyor belt sounds
    please make this loopable

--- hold-it!/src/conveyor/conveyor_belt.gd
+++ hold-it!/src/conveyor/conveyor_belt.gd
@@ -2,10 +2,13 @@ extends StaticBody2D
 class_name ConveyorBelt
 
 @onready var animPlayer: AnimationPlayer = $AnimationPlayer
+@onready var audioPlayer: AudioStreamPlayer2D = $AudioStreamPlayer2D
+var audio: AudioStreamWAV = load("res://src/conveyor/conveyor.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animPlayer.play("move")
+	audioPlayer.stream = audio
+	start_anim(true)
 	SignalBus.connect("hold_it", stop_anim)
 	SignalBus.connect("minigame_finished", start_anim)
 
@@ -16,5 +19,8 @@ func _process(delta: float) -> void:
 
 func start_anim(_passed):
 	animPlayer.play("move")
+	if not audioPlayer.playing:
+		audioPlayer.play()
 func stop_anim():
 	animPlayer.stop()
+	audioPlayer.stop()
