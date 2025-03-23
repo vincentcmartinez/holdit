21c45ecbdd7b262802d1bdabb1f53846dc3efaec
kedrickfudala <KedrickAFudala@gmail.com>
2025-03-23 16:24:36 -0400

    more visual fixes

--- hold-it!/cursor.gd
+++ hold-it!/cursor.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 class_name Cursor
 
 func _physics_process(delta: float) -> void:
-	global_position = get_global_mouse_position()
+	global_position = get_global_mouse_position() + Vector2(11,11)
 	if Input.is_action_pressed("left_click"):
 		frame = 1
 	else:
