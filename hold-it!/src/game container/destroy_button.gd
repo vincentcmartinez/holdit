5cbc9b0fe2c26502145907284c420d9d8f35e567
Catherine Xu <phf5kw@virginia.edu>
2025-03-23 12:00:28 -0400

    trash press collide animation

---
+++ hold-it!/src/game container/destroy_button.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var sprite : AnimatedSprite2D 
+@onready var paused : bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite = $AnimatedSprite2D
+	SignalBus.connect("destroy_finished", _on_destroy_finished)
+		
+func _input(event):
+	if(event.is_action_pressed("space") && !get_tree().paused && !paused):
+		activate_destroy()
+	if(event.is_action_pressed("left_click") && !get_tree().paused && !paused):
+		var pos_diff = abs(get_viewport().get_mouse_position() - sprite.global_position)
+		if(pos_diff.x <= 32 and pos_diff.y <= 32):
+			activate_destroy()
+
+func _on_destroy_finished():
+	paused = false
+	
+func activate_destroy():
+	sprite.play("pressed")
+	SignalBus.emit_signal("activate_destroy")
+	paused = true
--- hold-it!/src/game container/signalbus.gd
+++ hold-it!/src/game container/signalbus.gd
@@ -2,6 +2,8 @@ extends Node
 
 signal fix_button_pressed(part:ToyPart)
 signal hold_it
+signal activate_destroy
+signal destroy_finished
 signal minigame_started
 signal minigame_finished
 signal life_lost
